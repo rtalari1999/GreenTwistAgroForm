1c47d95fa2cb6e2af2bf19616138a698dbe19fcd
Raju Talari <rtalari1999@gmail.com>
2024-05-29 01:24:40 +0530

    basic implementation

--- GreenTwist/src/app/app.component.ts
+++ GreenTwist/src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { StylesComponent } from './styles/styles.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashboardComponent, StylesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'GreenTwist';
+  userName: string = '';
+  isUserName: boolean = true;
+  isSearch: boolean = false;
+  
+// ngif example
+user(){
+  if(this.userName != ''){
+    this.isSearch = true;
+  }
+  else{
+    this.isSearch = false;
+  }
+}
+//object
+courses = [
+  { id: 1, name: "Angular For Beginners" },
+  { id: 2, name: "Angular Core Deep Dive" },
+  { id: 3, name: "Angular Forms In Depth" },
+];
+// for loop track
+trackCourse(index: number, course: any) {
+  return course.id;
+}
 }
---
+++ GreenTwist/src/app/styles/styles.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-styles',
+  standalone: true,
+  imports: [],
+  templateUrl: './styles.component.html',
+  styleUrl: './styles.component.scss'
+})
+export class StylesComponent {
+
+}
